6689a5abdfc2b024d155b63e9d507bad2235cf55
marsimjob <mario.jobb@gmail.com>
2025-09-01 15:05:00 +0200

    New File Added caleld AdditionalContent

---
+++ AdditionalContent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstGitHubExercise
+{
+    internal class AdditionalContent
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This new infomration is important");
+        }
+    }
+}
